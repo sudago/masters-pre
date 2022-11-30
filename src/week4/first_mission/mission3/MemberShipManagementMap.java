ad47ac6e54cf19b3e2b25ac1964bd6cb61bb5b9b
sudago <sudago22@gmail.com>
2022-11-30 20:22:13 +0900

    주석 삭제 및 파일 이동

--- src/week4/first_mission/mission3/MemberShipManagementMap.java
+++ src/week4/first_mission/mission3/MemberShipManagementMap.java
@@ -5,19 +5,19 @@ import week3.first_mission.mission1.Member;
 import week3.first_mission.mission1.PlatinumMember;
 import week3.first_mission.mission1.RedMember;
 
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class MemberShipManagementMap {
-    private HashSet<Member> hashSet;
+    private HashMap<Integer, Member> hashMap;
     private Member member;
 
-    public MemberShipManagementMap(){this.hashSet = new HashSet<Member>();}
+    public MemberShipManagementMap(){
+        hashMap = new HashMap<Integer, Member>();
+    }
 
     public void addMemberShip(int memberId, String memberName, String memberShip){
-        /* GRADE 클래스의 인스턴스 변수에 new 생성자 사용시.
-        public void addMemberShip(int memberId, String memberName, String memberShip){
-        member = memberShip; // GRADE 안에 new 생성자 있음.
-        */
+
         // 인스턴스 추가를 위한 코드 수정. 아래의 코드는 GRADE 클래스 안에 new 생성자를 사용하지 않음.
         if (memberShip.equals("Red")) {
             member = new RedMember();
@@ -30,45 +30,32 @@ public class MemberShipManagementMap {
         member.setMemberId(memberId);
         member.setMemberName(memberName);
 
-        // 인스턴스를 추가했는데, hashSet으로 아이디, 이름이 완전히 똑같은 Member를 넣어도 추가되는게 문제임.
-        // Member 클래스에서 equals()와 hashCode() Override 재정의 했더니 문제 해결.
-        hashSet.add(member);
+        // key값이 이미 들어있는지 확인
+        if(hashMap.containsKey(memberId)) {
+            System.out.println("이미 있는 아이디 " + memberId + "의 값이 변경됩니다.");
+        }
+        hashMap.put(memberId, member);
 
-//        if (hashSet.add(member)) { // 일치하는 멤버가 없으면 추가.
-//            hashSet.add(member);
-//        } else { // 일치하는 멤버가 있으면 추가 못함.
-//            System.out.println("이미 있는 아이디 " + memberId + "는 추가할 수 없습니다.");
-//        }
     }
 
     public void showAllMember(){
         System.out.println("================================");
-        for (Member member : hashSet) {
-            System.out.println(member.toString());
+        Iterator<Integer> ir = hashMap.keySet().iterator();
+
+        while(ir.hasNext()) {
+            int key = ir.next();
+
+            Member member = hashMap.get(key);
+            System.out.println(member);
         }
         System.out.println("================================");
     }
 
     public boolean removeMember(int memberId){
-        // iterator를 돌릴 수 있음.
-        /*
-            Iterator<Member> iterator = hashSet.iterator();
-            while(iterator.hashNext()) {
-                Member member = iterator.next();
-                int tempId = member.getMemberId();
-                if (memberId == tempId) {
-                    hash.remove(member);
-                    return true;
-                }
-            }
-            System.out.println(memberId + "에 해당하는 멤버가 없습니다.");
-            return false;
-        */
-        for(Member member : hashSet) {
-            if (member.getMemberId() == memberId) {
-                hashSet.remove(member);
-                return true;
-            }
+  
+        if(hashMap.containsKey(memberId)) {
+            hashMap.remove(memberId);
+            return true;
         }
         System.out.println(memberId + "에 해당하는 멤버가 없습니다.");
         return false;
