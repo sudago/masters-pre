14c831c7007d6ba55c868f674634b155c172e041
sudago <sudago22@gmail.com>
2022-12-05 00:59:14 +0900

    set -> arrayList, 전역변수 사용x.
    구현 사항에 맞춰서 수정함.

--- src/study/baseball/BaseballGame.java
+++ src/study/baseball/BaseballGame.java
@@ -1,32 +1,33 @@
 package study.baseball;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 
 public class BaseballGame {
-    static HashSet<Integer> answerSet = new HashSet<>();
-    static String[] answerArray = new String[3];
-    String[] inputArray = new String[3];
-    int strike = 0;
-    int ball = 0;
 
     public BaseballGame(){}
 
     // 랜덤하게 3자리 수 정하기.
-    // 1. set을 사용해서 중복 숫자 제거하기.
-    public static void setGameNumber(){
-        while (answerSet.size() < 3) {
-            answerSet.add(((int)(Math.random()*9 + 1)));
+    // 1. 중복 숫자 제거하기.
+    public String[] setGameNumber(){
+        ArrayList<Integer> answerList = new ArrayList<>();
+        answerList.add((int)(Math.random()*9 + 1));
+        while (answerList.size() < 3) { // 서로 다른 수를 넣기 위한 while 문
+            int temp = (int)(Math.random()*9 + 1);
+            if (!answerList.contains(temp)) { // answerList 가 temp 를 갖고 있지 않으면
+                answerList.add(temp); // 랜덤한 수를 추가하기.
+            }
         }
-        setToArray();
+        return setToArray(answerList);
     }
-    // 2. set -> array에 담기.
-    public static void setToArray(){
-        int i = 0;
-        for (int number : answerSet) {
-            answerArray[i] = Integer.toString(number);
-            i++;
+
+    // 2. ArrayList -> Array 에 담기.
+    public String[] setToArray(ArrayList<Integer> answerList){
+        String[] answerArray = new String[3];
+        for (int i = 0; i < 3; i++) {
+            answerArray[i] = Integer.toString(answerList.get(i));
         }
+        return answerArray;
     }
 
     // 안내하기
@@ -34,49 +35,29 @@ public class BaseballGame {
         System.out.print("숫자를 입력해 주세요 ex) 123 : ");
     }
 
-    // 문자열로 받은 input을 배열로 나누기.
-    public void setInputNumber(String input) {
-        System.out.println(input);
-        inputArray = input.split("");
-    }
-
     // 룰 적용하기.
-    // 1. 스트라이크 비교하기
-    public void compareToAnswerNumber(){ // ✅한가지 일만 하도록 어떻게 하지??
-        for (int i = 0; i < 3; i++) {
-            if (answerArray[i].equals(inputArray[i])) {
-                strike++;
-            }
-            // ball 카운트는 다른 메서드로 빼 줌 (제약사항)
-            countBall(i);
-        }
-        // strike된 숫자도 ball에 count되서, strike를 센 만큼 빼준다.
-        ball -= strike;
-    }
-
-    // 2. 볼 세주기.
-    public void countBall(int i) {
+    public int countBall(String[] answerList, String[] inputArray, int i) {
+        int count = 0;
         for (int j = 0; j < 3; j++) {
-            if (answerArray[i].equals(inputArray[j])) {
-                ball++;
+            if (answerList[i].equals(inputArray[j])) {
+                count++;
             }
         }
+        return count;
     }
 
     // 정답 보여주기.
     // 1. 정답을 맞췄을 때
-    public Boolean showResult() {
-        if (Arrays.toString(answerArray).equals(Arrays.toString(inputArray))) {
+    public Boolean showResult(String[] answerList, String[] inputArray) {
+        if (Arrays.toString(answerList).equals(Arrays.toString(inputArray))) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             return true;
-        } else {
-            showStrikeAndBallInfo();
-            return false;
         }
+        return false;
     }
 
     // 2. 정답이 아닐 때.
-    public void showStrikeAndBallInfo(){
+    public void showStrikeAndBallInfo(int strike, int ball){
         if (strike == 0 && ball == 0) {
             System.out.println("낫싱");
         } else if (strike > 0 && ball > 0) {
@@ -86,6 +67,5 @@ public class BaseballGame {
         } else if (strike == 0 && ball > 0) {
             System.out.println(ball + "볼");
         }
-        System.out.println("정답: " + String.join("", answerArray)); // 랜덤 숫자 출력
     }
 }
--- src/study/baseball/BaseballGameTest.java
+++ src/study/baseball/BaseballGameTest.java
@@ -1,30 +1,45 @@
 package study.baseball;
 
-import java.util.Scanner;// 컴퓨터가 서로 다른 임의의 수 3개 선택.
-// 내가 숫자를 제시하면 메세지를 출력하고 다시 시도.
-// 내가 무한히 시도하다가, 맞추면 종료하는 방법.
+import java.util.Scanner;
+/*
+1. 컴퓨터가 서로 다른 임의의 수 3개 선택.
+2. 내가 숫자를 제시하면 strike, ball 을 출력하고, 틀렸다면 다시 시도.
+3. 무한히 시도하다가, 맞추면 종료하기.
+ */
 
 public class BaseballGameTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        BaseballGame baseballGame = new BaseballGame();
         // scanner로 입력 받을 때 마다, answerNumber가 바뀌지 않게 하기.
-        BaseballGame.setGameNumber();
-        // 정답을 맞출 때 까지 입력을 받기.
-        while(scanner.hasNext()) {
-            BaseballGame game = new BaseballGame();
-            game.startGame();
+        String[] answerArray = baseballGame.setGameNumber();
+        baseballGame.startGame(); // 안내 메세지 출력.
 
+        // 정답을 맞출 때 까지 입력 받기.
+        while(scanner.hasNext()) {
             String input = scanner.next();
-            // 입력받은 문자열 -> 배열로 바꾸기.
-            game.setInputNumber(input);
+//            System.out.println(input);
+            String[] inputArray = input.split(""); // 입력받은 문자열 -> 배열로 바꾸기.
 
             // 룰 적용하기.
-            game.compareToAnswerNumber();
+            int strike = 0;
+            int ball = 0;
+            // 1. 스트라이크 카운트 하기
+            for (int i = 0; i < 3; i++) {
+                if (answerArray[i].equals(inputArray[i])) {
+                    strike++;
+                }
+                // 2. ball 카운트 하기
+                ball += baseballGame.countBall(answerArray, inputArray, i);
+            }
+            ball -= strike;
 
             // 정답 출력하기.
-            if (game.showResult()) {
+            baseballGame.showStrikeAndBallInfo(strike, ball);
+            if (baseballGame.showResult(answerArray, inputArray)) {
                 break;
             }
+//            System.out.println("정답: " + String.join("", answerArray));
         } // while - end
 
         scanner.close();
